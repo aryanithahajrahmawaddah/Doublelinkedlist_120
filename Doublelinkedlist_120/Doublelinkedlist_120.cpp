316af57bc0aff15718c11775faf26a8c4a4c7935
aryanithahajrahmawaddah <aryanithahajrah@gmail.com>
2023-05-25 22:08:02 +1200

    mencetak data double linked list dari node yang dipilih

--- Doublelinkedlist_120/Doublelinkedlist_120.cpp
+++ Doublelinkedlist_120/Doublelinkedlist_120.cpp
@@ -136,3 +136,17 @@ void DoubleLinkedList::revtraverse() {
 }
 
 
+void DoubleLinkedList::hapus() {
+	if (listEmpty()) {
+		cout << "n\List is Empty" << endl;
+
+	}
+	cout << "\nEnter the roll number of the student whose record is to be deleted: ";
+	int rollNo;
+	cin >> rollNo;
+	cout << endl;
+	if (DoubleLinkedList::deleteNode(rollNo) == false)
+		cout << "Recor not found" << endl;
+	else
+		cout << "Record with roll number " << rollNo << "deleted" << endl;
+}
